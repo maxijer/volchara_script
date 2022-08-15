2819025068b2b89400cd32a4928294a9bfb91f69
maxijer <maxijer1234@yandex.ru>
2022-08-16 01:52:08 +0300

    add syntax analizator

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from lexer import Lexer
+from parser_ import Parser
 
 text_input = """
 auf(4 + 4 - 2);
 """
-
 lexer_ = Lexer().get_lexer()
 tokens = lexer_.lex(text_input)
 
-for token in tokens:
-    print(token)
+pg = Parser()
+pg.parse()
+parser = pg.get_parser()
+parser.parse(tokens).eval()
